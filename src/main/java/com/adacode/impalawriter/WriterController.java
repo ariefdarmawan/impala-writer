3ff709221c1dcc60552cabb58ca88c4ae7c8735b
Arief Darmawan <ariefda@hotmail.com>
2024-02-17 09:41:10 +0700

    bpo

--- src/main/java/com/adacode/impalawriter/WriterController.java
+++ src/main/java/com/adacode/impalawriter/WriterController.java
@@ -26,21 +26,42 @@ public class WriterController {
         return "OK";
     }
 
+    String sendStringToSQL(String dt) {
+        if (dt==null) return "null";
+        if (dt.startsWith("NULL")) return "null";
+        return String.format("'%s'", dt);
+    }
+
     @PostMapping("/v1/write")
     String write(@RequestBody BsiRecord record) {
         try {   
             Class.forName(ClassName);
             Connection conn = DriverManager.getConnection(Host);
             Statement stmt = conn.createStatement();
-            String sqlCmd = String.format("insert into comforte(as_of_date, cif_no) values('%s','%s')",
-                    record.as_of_date, record.cif_no);
+            String sqlCmd = String.format("insert into comforte(as_of_date, cif_no, cabangbsi, number_of_phr, "+
+            "bpo_1, bpo_2, bpo_3, bpo_4, bpo_5, bpo_6, bpo_7, bpo_8, bpo_9, bpo_10, ureg, usak, ziswaf) "+
+            "values('%s','%s','%s',%d, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %d, %d, %d)",
+                    record.as_of_date, record.cif_no, record.cabangbsi, record.number_of_phr, 
+                    sendStringToSQL(record.bpo_1), 
+                    sendStringToSQL(record.bpo_2), 
+                    sendStringToSQL(record.bpo_3), 
+                    sendStringToSQL(record.bpo_4), 
+                    sendStringToSQL(record.bpo_5), 
+                    sendStringToSQL(record.bpo_6), 
+                    sendStringToSQL(record.bpo_7), 
+                    sendStringToSQL(record.bpo_8), 
+                    sendStringToSQL(record.bpo_9), 
+                    sendStringToSQL(record.bpo_10), 
+                    record.ureg, record.usag, record.ziswaf);
             System.out.printf("execute command: %s\n",sqlCmd);
             stmt.execute(sqlCmd);
             conn.close();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
+            return "NOK";
         } catch (SQLException e) {
             e.printStackTrace();
+            return "NOK";
         }
 
         return "OK";
@@ -62,6 +83,21 @@ public class WriterController {
                 BsiRecord r = new BsiRecord();
                 r.as_of_date = rs.getDate("as_of_date");
                 r.cif_no = rs.getString("cif_no");
+                r.cabangbsi = rs.getNString("cabangbsi");
+                r.number_of_phr = rs.getInt("number_of_phr");
+                r.bpo_1 = rs.getString("bpo_1");
+                r.bpo_2 = rs.getString("bpo_2");
+                r.bpo_3 = rs.getString("bpo_3");
+                r.bpo_4 = rs.getString("bpo_4");
+                r.bpo_5 = rs.getString("bpo_5");
+                r.bpo_6 = rs.getString("bpo_6");
+                r.bpo_7 = rs.getString("bpo_7");
+                r.bpo_8 = rs.getString("bpo_8");
+                r.bpo_9 = rs.getString("bpo_9");
+                r.bpo_10 = rs.getString("bpo_10");
+                r.usag = rs.getByte("usag");
+                r.ureg = rs.getByte("ureg");
+                r.ziswaf = rs.getByte("ziswaf");
                 lst.add(r);
                 count++;
             }
